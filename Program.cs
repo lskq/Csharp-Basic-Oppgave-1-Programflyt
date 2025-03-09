bcf342ad8e11e482b7aec7fbc879eb046f2225d0
lskq <lskq012025@gmail.com>
2025-03-09 20:37:22 +0100

    Add functionality to Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"{args[0]}");
+        if (args.Length != 0)
+        {
+            foreach (string arg in args)
+            {
+                string message = $"{arg}: ";
+
+                if (double.TryParse(arg, out double temp))
+                {
+                    if (temp < -273.15)
+                    {
+                        message += "That's impossible!";
+                    }
+                    else if (temp <= 0)
+                    {
+                        message += "That's freezing!";
+                    }
+                    else if (temp >= 100)
+                    {
+                        message += "That's boiling!";
+                    }
+                    else
+                    {
+                        message += "That's neither freezing nor boiling.";
+                    }
+                }
+                else
+                {
+                    message += "That's not a valid temperature.";
+                }
+
+                Console.WriteLine(message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please provide one or more temperatures.");
+        }
     }
 }
